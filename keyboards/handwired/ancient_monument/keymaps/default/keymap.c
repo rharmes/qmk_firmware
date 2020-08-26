8882cf95c068ca7b8d7ce338bf4bcf224b767751
Ross Harmes <ross@rossharmes.net>
2020-08-25 20:12:19 -0700

    Add copyright and update the reference to the header file.

--- keyboards/handwired/ancient_monument/keymaps/default/keymap.c
+++ keyboards/handwired/ancient_monument/keymaps/default/keymap.c
@@ -1,5 +1,19 @@
+/* Copyright 2020 Ross Harmes <ross@rossharmes.net>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 #include QMK_KEYBOARD_H
-
 
 // Layout: http://www.keyboard-layout-editor.com/#/gists/aaa64d553c73dd6a8e15c7e91637d76c
 // Finger assignments: http://www.keyboard-layout-editor.com/#/gists/26b92acc628d7178145297d07cd5025a
