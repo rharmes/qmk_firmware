cb801cfce4807f2ab4612f06698b0f205c45aae7
Ross Harmes <ross@rossharmes.net>
2020-08-29 08:19:21 -0700

    Fix some typos so that the keyboard compiles (mostly missing commas).

--- keyboards/handwired/am96/rules.mk
+++ keyboards/handwired/am96/rules.mk
@@ -1,5 +1,30 @@
+# Copyright 2020 Ross Harmes <ross@rossharmes.net>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 # MCU name
 MCU = atmega32u4
+
+# Bootloader selection
+#     Teensy       halfkay
+#     Pro Micro    caterina
+#     Atmel DFU    atmel-dfu
+#     LUFA DFU     lufa-dfu
+#     QMK DFU      qmk-dfu
+#     ATmega32A    bootloadHID
+#     ATmega328P   USBasp
+BOOTLOADER = qmk-dfu
 
 # Processor frequency.
 #     This will define a symbol, F_CPU, in all source code files equal to the
@@ -14,7 +39,6 @@ MCU = atmega32u4
 #     software delays.
 F_CPU = 16000000
 
-#
 # LUFA specific
 #
 # Target architecture (see library "Board Types" documentation).
@@ -40,17 +64,19 @@ OPT_DEFS += -DINTERRUPT_CONTROL_ENDPOINT
 # Boot Section Size in *bytes*
 OPT_DEFS += -DBOOTLOADER_SIZE=4096
 
-
 # Build Options
 #   comment out to disable the options.
 #
-BOOTMAGIC_ENABLE ?= yes	# Virtual DIP switch configuration(+1000)
-MOUSEKEY_ENABLE ?= yes	# Mouse keys(+4700)
-EXTRAKEY_ENABLE ?= yes	# Audio control and System control(+450)
-CONSOLE_ENABLE ?= no	# Console for debug(+400)
-COMMAND_ENABLE ?= no    # Commands for debug and configuration
-SLEEP_LED_ENABLE ?= no  # Breathing sleep LED during USB suspend
-NKRO_ENABLE ?= yes		# USB Nkey Rollover - if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
-BACKLIGHT_ENABLE ?= no  # Enable keyboard backlight functionality
-AUDIO_ENABLE ?= no
-RGBLIGHT_ENABLE ?= yes
+BOOTMAGIC_ENABLE = yes # Virtual DIP switch configuration(+1000)
+MOUSEKEY_ENABLE  = yes # Mouse keys(+4700)
+EXTRAKEY_ENABLE  = yes # Audio control and System control(+450)
+CONSOLE_ENABLE   = no  # Console for debug(+400)
+COMMAND_ENABLE   = no  # Commands for debug and configuration
+SLEEP_LED_ENABLE = no  # Breathing sleep LED during USB suspend
+NKRO_ENABLE      = no  # USB Nkey Rollover - not yet supported in LUFA
+UNICODE_ENABLE   = yes # Enable support for arrow keys icon on the second layer.
+COMBO_ENABLE     = yes # Enable combo for special function when using multiple keys at once.
+TAP_DANCE_ENABLE = no  # Enable use multiple tap
+RGBLIGHT_ENABLE  = yes
+BACKLIGHT_ENABLE = no 
+AUDIO_ENABLE     = no
